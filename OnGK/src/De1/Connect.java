ae51933f7167baf27827532d3ecf12179d740e67
Nguyễn Văn Thưởng <89179543+ThuongWr19@users.noreply.github.com>
2025-04-13 16:37:18 +0700

    Update Connect.java

--- OnGK/src/De1/Connect.java
+++ OnGK/src/De1/Connect.java
@@ -7,23 +7,28 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Connect {
-    public static Connection getConnection() {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            return DriverManager.getConnection("jdbc:mysql://localhost/De1", "root", "");
-        } catch (ClassNotFoundException | SQLException ex) {
-            Logger.getLogger(Connect.class.getName()).log(Level.SEVERE, null, ex);
-            return null;
-        }
-    }
-
-    public static void close(Connection conn) {
-        if (conn != null) {
-            try {
-                conn.close();
-            } catch (SQLException ex) {
-                Logger.getLogger(Connect.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-    }
+	private static Connection conn = null;
+	public static Connection getConnection() {
+		
+		if(conn== null) {
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				conn = DriverManager.getConnection("jdbc:mysql://localhost/De1", "root", "");
+				
+			} catch (ClassNotFoundException | SQLException ex) {
+				Logger.getLogger(Connect.class.getName()).log(Level.SEVERE, null, ex);
+			}
+		}
+		return conn;
+	}
+	
+	public static void close() {
+		if(conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException ex) {
+				Logger.getLogger(Connect.class.getName()).log(Level.SEVERE, null, ex);
+			}
+		}
+	}
 }
